95bc3245a2a2dfd59a06109cf2bcbfb663728381
Prathyusha <tprathyushareddy31@gmail.com>
2025-03-17 08:16:17 +0530

    TestNG Annotations part 3 commit

--- src/test/java/ex_08_TestNG_Annotations_Examples/APITesting017_TestNG_dependsonMethods.java
+++ src/test/java/ex_08_TestNG_Annotations_Examples/APITesting017_TestNG_dependsonMethods.java
@@ -1,4 +1,18 @@
 package ex_08_TestNG_Annotations_Examples;
 
+import org.testng.annotations.Test;
+
 public class APITesting017_TestNG_dependsonMethods {
+    @Test(dependsOnMethods = "serverOn")
+    public void test_method1(){
+        System.out.println("Method 1");
+    }
+    @Test(dependsOnMethods = "serverOn")
+    public void test_method2(){
+        System.out.println("Method 2");
+    }
+    @Test
+    public void serverOn(){
+        System.out.println("Mandatory to execute first before others");
+    }
 }
--- src/test/java/ex_08_TestNG_Annotations_Examples/APITesting018_TestNG_Parameters.java
+++ src/test/java/ex_08_TestNG_Annotations_Examples/APITesting018_TestNG_Parameters.java
@@ -1,4 +1,17 @@
 package ex_08_TestNG_Annotations_Examples;
 
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
 public class APITesting018_TestNG_Parameters {
+    @Parameters("Browser")
+    @Test
+    public void invokeBrowser(String value){
+        if(value.equalsIgnoreCase("chrome")){
+            System.out.println("Invoke chrome browser");
+        } else if (value.equalsIgnoreCase("Firefox")) {
+            System.out.println("Invoke firefox browser");
+        }
+
+    }
 }
